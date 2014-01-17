0a70e8a1b65c038399bf61e40db6e22f19286bfd
Stanislav Vorobiov <s.vorobiov@samsung.com>
2014-01-17 17:40:36 +0400

    YaGL: Add more functions to GLESv3 dummy
    This is to make GFXBench compile. Also, add asserts
    so we can see what function is required and implement
    it
    
    Change-Id: I634501fec106cb5911af39c330127f480193a95d

--- GLESv2/dummy.c
+++ GLESv2/dummy.c
@@ -1,9 +1,11 @@
 #include "GLES3/gl3.h"
 #include "yagl_export.h"
 #include <stdlib.h>
+#include <assert.h>
 
 YAGL_API void glReadBuffer(GLenum mode)
 {
+    assert(0);
     exit(5);
 }
 
@@ -11,231 +13,288 @@ YAGL_API void glDrawRangeElements(GLenum mode, GLuint start, GLuint end,
                                   GLsizei count, GLenum type,
                                   const void *indices)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetBufferParameteri64v(GLenum target, GLenum pname, GLint64 *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetIntegeri_v(GLenum target, GLuint index, GLint *data)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetInteger64i_v(GLenum target, GLuint index, GLint64 *data)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glVertexAttribIPointer(GLuint index, GLint size, GLenum type, GLsizei stride, const void *pointer)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glClearBufferfv(GLenum buffer, GLint drawbuffer, const GLfloat *value)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glClearBufferiv(GLenum buffer, GLint drawbuffer, const GLint *value)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glClearBufferfi(GLenum buffer, GLint drawbuffer, GLfloat depth, GLint stencil)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glRenderbufferStorageMultisample(GLenum target, GLsizei samples, GLenum internalformat, GLsizei width, GLsizei height)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetInteger64v(GLenum pname, GLint64 *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API GLint glGetFragDataLocation(GLuint program, const GLchar *name)
 {
+    assert(0);
     exit(5);
     return 0;
 }
 
 YAGL_API void glGetProgramBinary(GLuint program, GLsizei bufSize, GLsizei *length, GLenum *binaryFormat, void *binary)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glProgramBinary(GLuint program, GLenum binaryFormat, const void *binary, GLsizei length)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API GLsync glFenceSync(GLenum condition, GLbitfield flags)
 {
+    assert(0);
     exit(5);
     return 0;
 }
 
 YAGL_API GLboolean glIsSync(GLsync sync)
 {
+    assert(0);
     exit(5);
     return 0;
 }
 
 YAGL_API void glDeleteSync(GLsync sync)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API GLenum glClientWaitSync(GLsync sync, GLbitfield flags, GLuint64 timeout)
 {
+    assert(0);
     exit(5);
     return 0;
 }
 
 YAGL_API void glWaitSync(GLsync sync, GLbitfield flags, GLuint64 timeout)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetSynciv(GLsync sync, GLenum pname, GLsizei bufSize, GLsizei *length, GLint *values)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glTexStorage2D(GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glUniformMatrix2x3fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGenSamplers(GLsizei count, GLuint *samplers)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glDeleteSamplers(GLsizei count, const GLuint *samplers)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API GLboolean glIsSampler(GLuint sampler)
 {
+    assert(0);
     exit(5);
     return 0;
 }
 
 YAGL_API void glBindSampler(GLuint unit, GLuint sampler)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glSamplerParameteri(GLuint sampler, GLenum pname, GLint param)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glSamplerParameteriv(GLuint sampler, GLenum pname, const GLint *param)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glSamplerParameterf(GLuint sampler, GLenum pname, GLfloat param)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glSamplerParameterfv(GLuint sampler, GLenum pname, const GLfloat *param)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetVertexAttribIiv(GLuint index, GLenum pname, GLint *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glClearBufferuiv(GLenum buffer, GLint drawbuffer, const GLuint *value)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetVertexAttribIuiv(GLuint index, GLenum pname, GLuint *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glVertexAttribI4i(GLuint index, GLint x, GLint y, GLint z, GLint w)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glVertexAttribI4ui(GLuint index, GLuint x, GLuint y, GLuint z, GLuint w)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glVertexAttribI4iv(GLuint index, const GLint *v)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glVertexAttribI4uiv(GLuint index, const GLuint *v)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetUniformuiv(GLuint program, GLint location, GLuint *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glUniform2ui(GLint location, GLuint v0, GLuint v1)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glUniform2uiv(GLint location, GLsizei count, const GLuint *value)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API const GLubyte *glGetStringi(GLenum name, GLuint index)
 {
+    assert(0);
     exit(5);
     return NULL;
 }
 
 YAGL_API void glCopyBufferSubData(GLenum readTarget, GLenum writeTarget, GLintptr readOffset, GLintptr writeOffset, GLsizeiptr size)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetSamplerParameteriv(GLuint sampler, GLenum pname, GLint *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glGetSamplerParameterfv(GLuint sampler, GLenum pname, GLfloat *params)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glInvalidateSubFramebuffer(GLenum target, GLsizei numAttachments, const GLenum *attachments, GLint x, GLint y, GLsizei width, GLsizei height)
 {
+    assert(0);
     exit(5);
 }
 
 YAGL_API void glProgramParameteri(GLuint program, GLenum pname, GLint value)
 {
+    assert(0);
+    exit(5);
+}
+
+YAGL_API void glTexStorage3D(GLenum target, GLsizei levels, GLenum internalformat, GLsizei width, GLsizei height, GLsizei depth)
+{
+    assert(0);
+    exit(5);
+}
+
+YAGL_API void glGetInternalformativ(GLenum target, GLenum internalformat, GLenum pname, GLsizei bufSize, GLint *params)
+{
+    assert(0);
     exit(5);
 }
