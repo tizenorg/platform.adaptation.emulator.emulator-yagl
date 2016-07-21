26cba2fbec0f0086397cf876a1f7bc273f3b8acf
Vasiliy Ulyanov <v.ulyanov@samsung.com>
2016-07-21 15:58:28 +0300

    YaGL: Check for GPU acceleration on boot
    If emulator is run without host GPU acceleration, yagl will likely
    terminate GL apps on initialization failure. In order to handle the
    situation propely, dummy GL libs are now packed along with yagl. When
    emulator boots up, a script will check if /dev/yagl is present and
    setup the symlinks appropriately.
    
    Change-Id: I6715c5798b0c2a6cdef16a24a1e3c8786f6aa36f
    Signed-off-by: Vasiliy Ulyanov <v.ulyanov@samsung.com>

--- packaging/opengl-es-setup-yagl-env.sh
+++ packaging/opengl-es-setup-yagl-env.sh
@@ -1,45 +1,14 @@
- #!/bin/sh
+#!/bin/sh
 
-echo -e "[${_G} Opengl-es acceleration module setting. ${C_}]"
+echo -e "[${_G} OpenGL ES acceleration module setting. ${C_}]"
 if [ -e /dev/yagl ] ; then
-    echo -e "[${_G} Emulator support gles hw acceleration. ${C_}]"
-    echo -e "[${_G} Change permission of /dev/yagl. ${C_}]"
-    chown root:video /dev/dri/card0
-    chown root:video /dev/yagl
-    chmod 660 /dev/dri/card0
-    chmod 660 /dev/yagl
-    chsmack -a "*" /dev/dri/card0
-    chsmack -a "*" /dev/yagl
-    echo -e "[${_G} Apply to use hw gles library. ${C_}]"
-    ln -s -f /usr/lib/yagl/libEGL.so.1.0 /usr/lib/libEGL.so
-    ln -s -f /usr/lib/yagl/libEGL.so.1.0 /usr/lib/libEGL.so.1
-    ln -s -f /usr/lib/yagl/libGLESv1_CM.so.1.0 /usr/lib/libGLESv1_CM.so
-    ln -s -f /usr/lib/yagl/libGLESv1_CM.so.1.0 /usr/lib/libGLESv1_CM.so.1
-    ln -s -f /usr/lib/yagl/libGLESv2.so.1.0 /usr/lib/libGLESv2.so
-    ln -s -f /usr/lib/yagl/libGLESv2.so.1.0 /usr/lib/libGLESv2.so.1
-elif grep "yagl=1" /proc/cmdline ; then
-    echo -e "[${_G} Emulator support gles hw acceleration. ${C_}]"
-    echo -e "[${_G} Change permission of /dev/yagl. ${C_}]"
-    chown root:video /dev/dri/card0
-    chown root:video /dev/yagl
-    chmod 660 /dev/dri/card0
-    chmod 660 /dev/yagl
-    chsmack -a "*" /dev/dri/card0
-    chsmack -a "*" /dev/yagl
-    echo -e "[${_G} Apply to use hw gles library. ${C_}]"
-    ln -s -f /usr/lib/yagl/libEGL.so.1.0 /usr/lib/libEGL.so
-    ln -s -f /usr/lib/yagl/libEGL.so.1.0 /usr/lib/libEGL.so.1
-    ln -s -f /usr/lib/yagl/libGLESv1_CM.so.1.0 /usr/lib/libGLESv1_CM.so
-    ln -s -f /usr/lib/yagl/libGLESv1_CM.so.1.0 /usr/lib/libGLESv1_CM.so.1
-    ln -s -f /usr/lib/yagl/libGLESv2.so.1.0 /usr/lib/libGLESv2.so
-    ln -s -f /usr/lib/yagl/libGLESv2.so.1.0 /usr/lib/libGLESv2.so.1
+    echo -e "[${_G} Emulator supports gles hw acceleration. ${C_}]"
+    ln -s -f libEGL.so.1.0 /usr/lib/driver/libEGL.so.1
+    ln -s -f libGLESv1_CM.so.1.0 /usr/lib/driver/libGLESv1_CM.so.1
+    ln -s -f libGLESv2.so.2.0 /usr/lib/driver/libGLESv2.so.2
 else
     echo -e "[${_G} Emulator does not support gles hw acceleration. ${C_}]"
-    echo -e "[${_G} Apply to use gles stub library. ${C_}]"
-    ln -s -f /usr/lib/dummy-gl/libEGL_dummy.so /usr/lib/libEGL.so
-    ln -s -f /usr/lib/dummy-gl/libEGL_dummy.so /usr/lib/libEGL.so.1
-    ln -s -f /usr/lib/dummy-gl/libGLESv1_dummy.so /usr/lib/libGLESv1_CM.so
-    ln -s -f /usr/lib/dummy-gl/libGLESv1_dummy.so /usr/lib/libGLESv1_CM.so.1
-    ln -s -f /usr/lib/dummy-gl/libGLESv2_dummy.so /usr/lib/libGLESv2.so
-    ln -s -f /usr/lib/dummy-gl/libGLESv2_dummy.so /usr/lib/libGLESv2.so.1
+    ln -s -f libEGL_dummy.so /usr/lib/driver/libEGL.so.1
+    ln -s -f libGLESv1_dummy.so /usr/lib/driver/libGLESv1_CM.so.1
+    ln -s -f libGLESv2_dummy.so /usr/lib/driver/libGLESv2.so.2
 fi
